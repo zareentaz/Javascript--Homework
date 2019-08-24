3c96fef2cb51130631b3ff9dcd2923cdb024931a
zareen <zareentazshukur@gmail.com>
2019-08-23 19:55:07 -0500

    code for filtering data by shape

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,33 +12,55 @@ var submit = d3.select("#filter-btn");
 
 submit.on("click", function() {
 
-  // Prevent the page from refreshing
+    // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
+  // Select the date-input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
+  // Get the value property of the date input element
   var inputValue = inputElement.property("value");
 
-  var inputcity =d3.select("#cityname");
+   // Select the city input element and get the raw HTML node
+  var inputcity =d3.select("#city");
 
+  // Get the value property of the date input element
   var input_city_value = inputcity.property("value");
+
+  // Select the state input element and get the raw HTML node
+  var inputstate =d3.select("#state");
+
+  // Get the value property of the state input element
+  var input_state_value = inputstate.property("value");
+
+  // Select the country input element and get the raw HTML node
+  var inputcountry =d3.select("#country");
+
+  // Get the value property of the country input element
+  var input_country_value = inputcountry.property("value");
+
+
+  // Select the shape input element and get the raw HTML node
+  var inputshape =d3.select("#shape");
+
+  // Get the value property of the shape input element
+  var input_shape_value = inputshape.property("value");
+
+
 
 
   // filtering the data on the basis of user input
 
   var filteredData = tableData.filter(info => info.datetime === inputValue);
   var filtercity = tableData.filter(info => info.city === input_city_value);
+  var filterstate = tableData.filter(info => info.state === input_state_value);
+  var filtercountry = tableData.filter(info => info.country === input_country_value);
+  var filtershape = tableData.filter(info => info.shape === input_shape_value);
 
-  console.log(filteredData);
-  console.log(filtercity);
 
+    
   
-  // Then, select the unordered list element by class name
-
-  
-  // comment to clear the body which giving next search
+  // comment to clear the body while giving next search
   document.getElementsByTagName('tbody')[0].innerHTML = '';
 
   //append one table row `tr` for each alliendata object
@@ -47,7 +69,7 @@ submit.on("click", function() {
     var row=tbody.append("tr");
 
   //Use `Object.entries` to console.log each alliendata value
-   Object.entries(Alliendata).forEach(function([key, value]){
+    Object.entries(Alliendata).forEach(function([key, value]){
     // console.log(key, value);
 
      //Append a cell to the row for each value
@@ -55,8 +77,32 @@ submit.on("click", function() {
      cell.text(value);
     });
   });
+
+
+  // comment to clear the body while giving next search
+ //document.getElementsByTagName('tbody')[0].innerHTML = '';
   
+//append the data based on the city search
   filtercity.forEach(function(Alliendata){
+
+     var row=tbody.append("tr");
+
+  //Use `Object.entries` to console.log each alliendata value
+   Object.entries(Alliendata).forEach(function([key, value]){
+    // console.log(key, value);
+
+     //Append a cell to the row for each value
+     var cell = row.append("td");
+     cell.text(value);
+
+   });
+  });
+
+  
+  
+  //append the data based on the state search
+  filterstate.forEach(function(Alliendata){
+
 
     var row=tbody.append("tr");
 
@@ -69,4 +115,43 @@ submit.on("click", function() {
      cell.text(value);
    });
   });
+
+  
+
+  //append the data based on the country search
+  filtercountry.forEach(function(Alliendata){
+
+    var row=tbody.append("tr");
+
+  //Use `Object.entries` to console.log each alliendata value
+   Object.entries(Alliendata).forEach(function([key, value]){
+    // console.log(key, value);
+
+     //Append a cell to the row for each value
+     var cell = row.append("td");
+     cell.text(value);
+
+   });
+  });
+
+
+  
+  //append the data based on the shape search
+  filtershape.forEach(function(Alliendata){
+
+    var row=tbody.append("tr");
+
+  //Use `Object.entries` to console.log each alliendata value
+   Object.entries(Alliendata).forEach(function([key, value]){
+    // console.log(key, value);
+
+     //Append a cell to the row for each value
+     var cell = row.append("td");
+     cell.text(value);
+   });
+   
+  });
+   
 });
+
+
